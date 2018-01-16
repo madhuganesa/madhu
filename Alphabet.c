2471e79b65ff332c6ee365f7134ff0d0d35c0b84
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-16 10:27:33 +0530

    Update Alphabet.c

--- Alphabet.c
+++ Alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <conio.h>
 {
-Char c;
-printf("enter a character":");
-scanf ("%c",&c);
-if(c>'a'&&c <='z')|(c>='A'&&c <='Z'))
-printf ("%cis an alphabet.",c);
-else
-printf("%c is not an alphabet."c);
-return0;
+   Char c;
+   printf("Enter a character":");
+   scanf ("%c",&c);
+          
+   if(c>'a'&&c <='z')||(c>='A'&&c <='Z'))
+      printf ("%cis an alphabet.",c);
+   else
+      printf("%c is not an alphabet."c);
+   return0;
 }
