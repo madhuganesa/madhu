0100c1107928eb04107697a01645accb925de63f
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-18 21:29:42 +0530

    Rename k.c to m.c

---
+++ m.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,sum=0;
+printf(" %d %d",n,k);
+scanf(" %d %d",&n &k);
+for(i=0;i<=0;i++)
+{
+printf("%d",a[i]);
+}
+for(i=1;i<=k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+return 0;
+}
