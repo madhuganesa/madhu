48aba0b2b958ae97ea65aa0d8d568b64255634d8
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-21 06:20:14 +0000

    Rename multiples.c to 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+voidmain()
+{
+int marks[5]={10,20,30,40,50};
+int i=0, counter=0;
+printf("enter a number less than5");
+scanf("%d",&counter);
+for(i=0;i<<counter;i++)
+{
+printf("%d\n,"marks[i]);
+}
+}
