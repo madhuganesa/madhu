d347d6249428e992ab8e696cdd0bf5efb195e168
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-21 06:45:14 +0000

    Create maxmium 33.c

---
+++ maxmium 33.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int[]array={a,b,c,d,e,f};
+printf("the contents of the array are:");
+for(int i=0;i<=10;i++)
+printf("/t/t/t array["+i+"]="array[i]);
+}
+}
