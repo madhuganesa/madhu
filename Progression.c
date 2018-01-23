84b3af660911baf892a82376ac087de8a90118e0
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-23 22:03:40 +0530

    Create Progression.c

---
+++ Progression.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#iclude<conio.h>
+int main()
+{
+  int a,d,n,i;
+   intsum=0;
+  printf("Enter the 1st term value of arithmetic progression series:");
+  scanf("%d,"&n);
+  printf("Enter total no in the arithmetic progression series:");
+  scanf("%d,"&n);
+  printf("Enter the common difference of arithmetic progression series:");
+  scanf("%d,"&n);
+  sum=(n*(2*a+(n-1)*d()/2;
+  tn=a+(n-1)*d;
+  printf("sum of the arithmetic progression series:");
+  for(i=a;i<n;i=i+d)
+  {
+    if(i!=n)
+        printf("%d+,"i);
+    else
+        printf("%d=%d,"i,sum);
+   }
+    return 0;
+ }
