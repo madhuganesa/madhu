ddcf44378169c07d8e71ce1f4864b5165354e3e7
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-24 16:24:01 +0530

    Create N.c

---
+++ N.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define max size 1000
+int main()
+{
+int arr[max.size];
+int i,n;
+printf("enter size of array:");
+scanf("%d",&n);
+printf("enter %d elements in the array:",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("\n element in array are:");
+for(i=0;i<n;i++)
+{
+printf("%d,"arr[i]);
+}
+return0;
+}
