9bb49feebab36ace2d91d9598ec6c459b03bf41a
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-24 16:15:09 +0530

    Create sorted.c

---
+++ sorted.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int array[];
+int a,b,num,temp;
+printf("enter the value of number\n);
+scanf("%d",&num);
+printf("%d",&num);
+printf("enter the elements one by one\n");
+for(i=0;i<num;i++)
+{
+scanf("%d",&array [a]);
+}
+printf("input array is \n");
+for(i=0;i<num;i++)
+{
+for(b=0;b<(num a-1);b++)
+{
+for(b=0;b<(num-a-1);b++)
+}
+}
