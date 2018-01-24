e65b993000ea6dfc722c54f3c927f856757af6c7
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-24 16:45:49 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int hours minutes,total_minutes;
+printf(\n enter the time in hours and minutes:\n");
+printf(\n hours:\t");
+scanf("%d",& hours);
+printf(\n minutes:\t);
+scanf("%d",&minutes);
+total_minutes=(hours*60)+minutes;
+printf("\n total time in minutes:\t%d\n,"total_minute);
+return 0;
+}
