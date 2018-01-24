a0360ae4fb89450caed13206cf658fe339051546
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-24 16:38:05 +0530

    Create hour.c

---
+++ hour.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int hour,minute1,total;
+printf("hour":);
+if(scanf("%d",&hour)==1)
+{
+printf("minute":);
+if(scanf("%d",&minute)==1)
+{
+minute1=hour*60:
+total=minute1+minute;
+printf("total minute=>%d",total);
+}
+else
+{
+printf("error ,enter correct value");
+}
+else
+{
+printf("error,enter correct value");
+}
+}
