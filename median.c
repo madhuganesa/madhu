c3b7c666233427536c00753393b0119b5072fbc1
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-24 16:03:34 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int a[3],i,n=3;
+float m;
+printf("enter the elements:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+if(n%2==0)
+{
+m=(a[n-1/2]+(a[n/2]))/2
+{
+else
+{
+m=a[n/2];
+}
+printf("\n median is%f",m);
+getch();
+}
+
+
