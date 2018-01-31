d51b32bbd749d7ace7021cf989f5d91d12563abf
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-31 16:57:05 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a=13,b,c;
+	printf("enter the number");
+	scanf("%d",&b);
+	if(a!=0)
+	{
+		if(c>a)
+		{
+			printf("multiply %d",c);
+			}
+			else
+			{
+				printf(" no multiply %d",c);
+				}
+				
+	}
+	return 0;
+	
+}
