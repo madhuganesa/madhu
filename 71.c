c1db7eb703c00cf48dd45075f7538d36242b8b70
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-01-31 15:51:34 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,25 @@
+#include<stdio.h
+#include<conio.h>
+void main()
+{
+int n,a,r,s=0;
+clrscr();
+printf("\n enter the number:");
+scanf("%d",&n);
+a=n;
+while(n>0)
+{
+r=n%10;
+s=s*10+r;
+n=n/10;
+}
+if(a==s)
+{
+printf("\n%d is a palindrome number,"a);
+}
+else
+{
+printf("\n %d is not a palindrome number,"a);
+}
+getch();
+}
