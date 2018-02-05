5c925037a73b7ffdb3b03dc02c93d635b1f002bd
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-05 16:06:16 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the value of a and b");
+scanf("%d %d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+printf("the value is even %d",c);
+}
+else
+{
+printf("the value is odd %d",c);
+}
+return 0;
+}
