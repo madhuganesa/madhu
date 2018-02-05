0b358047159893b522734db7a639817b032a1ce5
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-05 16:41:47 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int m,char a[20],b[20];
+printf("enter the value of a and b");
+scanf("% %s",&a,&b);
+m=strlen(a);
+n=strlen(b);
+if(m>=n0
+{
+printf("%s",a0;
+}
+else
+{
+printf("%s",b);
+}
+return 0;
+}
