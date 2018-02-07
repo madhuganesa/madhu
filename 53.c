b1fae8354c8dec4befcd718ecb24382681427166
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-07 16:55:48 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a, sum=0;
+printf("enter the value");
+scanf("%d",&a);
+for(a=0;a<4;a++)
+{
+sum= sum+a;
+}
+printf("sum of digits");
+return 0;
+}
+
