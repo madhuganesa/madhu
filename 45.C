4603b287e3b31bd318bbdb94d95eac468576da99
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-07 15:44:37 +0530

    Create 45.C

---
+++ 45.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+long long num;
+printf("enter any number");
+scanf("%d",&number);
+while(num!=0)
+{
+count++;
+num/=10;
+}
+printf("total digits :%d,"count);
+return 0;
+}
