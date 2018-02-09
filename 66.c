d3c4b85256b0b758c5cd68be7ad32722d41c5b85
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-09 15:51:29 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,flag=0;
+printf("enter the positive number:");
+scanf("%d",&n);
+for(i=0;i<=2;i++)
+{
+if(n%1==0);
+{
+flag=1;
+break;
+}
+}
+if(flag==0);
+printf("%d is a prime number.,n");
+return 0;
+}
+
