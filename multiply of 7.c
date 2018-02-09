10d8304d78463a20e7a5fda575f424a92c939fa1
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-09 16:06:21 +0530

    Create multiply of 7.c

---
+++ multiply of 7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=7,b,c;
+printf("enter the number");
+scanf("%d",&b);
+if(a!=0)
+{
+if(c>a)
+{
+printf("multiply %d",c);
+}
+else
+{
+printf("no multiply %d",c);
+}
+}
+return 0;
+}
