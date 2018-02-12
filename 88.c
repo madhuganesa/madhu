ac5ec57e295e211e3c35b6013664476469e393d1
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-12 16:41:09 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,n1,n2,lcm;
+    printf("enter the two number");
+    scanf("%d %d",&n1,&n2);
+    for(i=0;i<=n1&&n2;i++)
+    {
+        if(n1%i==0,&&n2%i==0)lcm;
+    }
+    return 0;
+}
