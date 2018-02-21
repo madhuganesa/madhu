590d996bdee53e3353d9525519a7efdaca36d755
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-21 16:28:57 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the limit");
+scanf("%d %d",&a&b);
+printf("enter the number should be checked");
+scanf("%d",&c);
+if((c<<a)\\(c<<b))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
