c5b775c733fcc05c298ac39db10a5ee7a1f564aa
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-26 16:01:43 +0530

    Update ave.c

--- ave.c
+++ ave.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 int main()
+{
 int p,q,r,s,t,u;
 printf("enter the number:");
 scanf("%d%d%d%d%d%d &p,&q,&r,&s,&t,&u);
 f=(p+q+r+s+t)/5;
 printf("the number is%d",u);
-return ;
-}0
+return 0;
+      }
