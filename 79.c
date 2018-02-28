d142bee219ad43e3caae4fb07f111ba091d2acba
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-28 16:57:19 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the two number");
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c==(a*a)&& c==(b*b));
+{
+        if("yes")
+}
+else
+{
+        if("no")
+}
+return 0;
+}
