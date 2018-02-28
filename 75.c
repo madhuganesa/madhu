8ee14ac2f9f5d58382643cc5ec39ffecaa94a7d9
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-02-28 16:27:21 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[40];
+printf("enter the number");
+intrlen;
+len=lenstr[str];
+if(len%2!=0)
+{
+        str=[(len-1)/2]='*';
+}
+else
+{
+        str=[len/2]='*';
+        str=[(len/2)-1]='*';
+}
+printf("%s",str);
+return 0;
+}
