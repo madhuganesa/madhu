325f0f21b84a35f246213eda4aeab03fa9c2b180
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-03-02 16:22:12 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int 1,f,i;
+scanf("%d",&f);
+int a[f];
+for(i=0;i<f;i++)
+{
+scanf("%d",&a[i]);
+1=a[0];
+}
+for(i=1;i<f;i++)
+{
+        if(1<a[i])
+{
+1=a[i];
+}
+}
+printf("%d",1);
+return 0;
+}
