84122397d7b8018eae4d258a860d875938f55518
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-03-19 16:18:23 +0530

    Create 90.C

---
+++ 90.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	char a[10];
+	int i;
+	scanf("%s",&a);
+	
+	for(i=0;a[i]!=NULL;i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+		printf("%c",a[i]);
+		}
+			}
+	return 0;
+}
