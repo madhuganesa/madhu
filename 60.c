ae2c449aa7f9dd3bdf761267e52d9b8d48e4246e
madhuganesa <35220708+madhuganesa@users.noreply.github.com>
2018-03-19 16:48:33 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int f1=0,f2=1,f,n,i;
+    scanf("%d",&n);
+    printf("%d\t%d\t",f1,f2);
+    for(i=2;i<n;i++)
+{     f=f1+f2;
+       f1=f2;
+       f2=f;
+       printf("%d\t",f);
+}
+return 0;
+}
+    
